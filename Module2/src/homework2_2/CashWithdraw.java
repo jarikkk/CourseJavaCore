4fd965559de607af14bb2576c88bb81304a97aef
yakovalenko <jaroslawkovalenko@jmail.com>
2016-08-26 11:34:38 +0300

    java learning

--- Module2/src/homework2_2/CashWithdraw.java
+++ Module2/src/homework2_2/CashWithdraw.java
@@ -1,7 +1,5 @@
 package homework2_2;
 
-import java.util.Arrays;
-
 public class CashWithdraw {
     static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
     static int[] balances = {1200, 250, 2000, 500, 3200};
@@ -10,81 +8,45 @@ public class CashWithdraw {
         int index = 0;
         for (int i = 0; i < ownerNames.length; i++) {
             if (ownerNames[i].equals(ownerName)) index = i;
-            
+
         }
         return index;
     }
 
+    static double searchBalance(int[] balances, int searchBalance) {
+        int balance = 0;
 
-    static int balancesSum(int[] balances) {
-        int sum = 0;
-        for (int balance : balances) {
-            if (balance > 1000 && balance < 5000) {
-                sum += balance;
-            }
-        }
-        return sum;
-    }
-
-    static String[] getOwnerNames(int[] balances, String[] names) {
-        int ownersCount = 0;
-        for (int balance : balances) {
-            if (balance > 1000) ownersCount++;
-        }
-
-        String[] result = new String[ownersCount];
-
-        int index = 0;
         for (int i = 0; i < balances.length; i++) {
-            if (balances[i] > 1000) {
-                result[index] = names[i];
-                index++;
-            }
+            if (i == searchBalance) balance = balances[i];
         }
-
-        return result;
+        return balance;
     }
 
-    ///*тип возвращаемого значения*/void  /*название метода*/ (/*параметры*/) {
-    //логика
-
-    //return //что-то
-    //}
-
-    int testMethod(int element, double test1, char pool) {
-        //logic
-        return 0;
-    }
-
-    void testMethod2(int test, String name) {
-        //logic
-        System.out.println("test....");
-
-
-    }
-
-    //параметры
-    //тип значения название параметра
-
-    //HOMEWORK 2.1
-    static double withdrawBalance(double balance, double withdrawal, double commision) {
+    //HOMEWORK 2.2
+    static double withdrawBalance(double balance, double withdrawal1, double commision) {
         double result1 = 0.;
 
-        double result2 = withdrawal * commision / 100;
-        result1 = balance - withdrawal - result2;
+        double result2 = withdrawal1 * commision / 100;
+        result1 = balance - withdrawal1 - result2;
 
         return result1;
     }
 
-    //HOMEWORK 2.2
-    static double withdrawBalance(String ownerName, double withdrawal, double commision) {
+    //HOMEWORK 2.3
+    static double withdrawBalance(String ownerName, double withdrawal2, double commision) {
         double result1 = 0;
         String name = ownerName;
-        //double result2 = withdrawal * commision / 100;
-        return 0;
+        int index = searchOwner(ownerName);
+        double balance2 = searchBalance(balances, index);
+
+        double result2 = withdrawal2 * commision / 100;
+        result1 = balance2 - withdrawal2 - result2;
+
+        return result1;
     }
 
-    //HOMEWORK 2.3
+
+    //HOMEWORK 2.4
     static double fundBalance(String ownerName, double fund) {
 
         return 0;
@@ -92,27 +54,28 @@ public class CashWithdraw {
 
     public static void main(String[] args) {
 
-        double balance = 100;
-        double withdrawal = 10;
+        double balance = 100;//task2 case
+        double withdrawal1 = 10;
         double commision = 5.;
-        double balanceAfter = withdrawBalance(balance, withdrawal, commision);
-        String ownerName = "Oww";
+        double balanceAfter = withdrawBalance(balance, withdrawal1, commision);
+
+        String ownerName = "Oww";//task3 case
+        double withdrawal2 = 100;
+        double balanceAfter2 = withdrawBalance(ownerName, withdrawal2, commision);
 
 
-        if (balanceAfter >= 0) {
+        if (balanceAfter >= 0) {//task2 case
             System.out.println("OK " + commision + " " + balanceAfter);
         } else {
             System.out.println("NO");
         }
 
-        System.out.println(searchOwner(ownerName));
+        if (balanceAfter2 >= 0) {//task3 case
+            System.out.println(ownerName + " " + withdrawal2 + " " + balanceAfter2);
+        } else {
+            System.out.println(ownerName + " NO");
+        }
 
-        //int[] balances = {1200, 250, 2000, 500, 3200};
-
-        // System.out.println(Arrays.toString(balances));
-
-
-        // int[] balances1 = {200, 200, 500, 4000, 30};
 
     }
 
