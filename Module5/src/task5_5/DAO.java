9666652ef411795aa9b2ae24ef9149122645e8d5
yakovalenko <jaroslawkovalenko@jmail.com>
2016-09-17 12:42:39 +0300

    Task 4

---
+++ Module5/src/task5_5/DAO.java
@@ -0,0 +1,13 @@
+package task5_5;
+
+import task5_1_and_5_2.Room;
+
+public interface DAO {
+
+    Room save(Room room);
+    boolean delete(Room room);
+    Room update(Room room);
+    Room findById(long id);
+
+
+}
