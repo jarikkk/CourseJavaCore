07e5af140e346c2282ba4fdbd484f6e32bdf8a27
yakovalenko <jaroslawkovalenko@jmail.com>
2016-09-25 21:42:27 +0300

    Task 6

---
+++ Module6/src/task6_5/UserUtils.java
@@ -0,0 +1,26 @@
+package task6_5;
+
+import task6_4.User;
+
+public class UserUtils {
+
+    User[] uniqueUsers(User[] users){
+        return null;
+    }
+
+    User[] usersWithContitionalBalance(User[] users, int balance){
+        return null;
+    }
+
+    final User[] paySalaryToUsers(User[] users){
+        return null;
+    }
+
+    final long[] getUsersId(User[] users){
+         return null;
+    }
+
+    User[] deleteEmptyUsers(User[] users){
+        return null;
+    }
+}
