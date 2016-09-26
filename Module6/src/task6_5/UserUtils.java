6c7a3e470b76e798f997da33899405d4851c7234
yakovalenko <jaroslawkovalenko@jmail.com>
2016-09-26 14:50:25 +0300

    Task 4

--- Module6/src/task6_5/UserUtils.java
+++ Module6/src/task6_5/UserUtils.java
@@ -4,23 +4,41 @@ import task6_4.User;
 
 public class UserUtils {
 
-    public User[] uniqueUsers(User[] users){
+    public User[] uniqueUsers(User[] users) {
+
+        User tmp = null;
+        int count = 0;
+        User[] uniqueUsersFound = new User[count];
+
+        for (int i = 0; i < users.length; i++) {
+            users[i] = tmp;
+            count++;
+            for (int j = 0; j < users.length; j++) {
+                if (!users[j].equals(tmp)) {
+                    uniqueUsersFound[i] = tmp;
+                    if (users[j].equals(tmp)) {
+                        tmp = null;
+                    }
+                }
+            }
+
+        }
+        return uniqueUsersFound;
+    }
+
+    User[] usersWithContitionalBalance(User[] users, int balance) {
         return null;
     }
 
-    User[] usersWithContitionalBalance(User[] users, int balance){
+    final User[] paySalaryToUsers(User[] users) {
         return null;
     }
 
-    final User[] paySalaryToUsers(User[] users){
+    final long[] getUsersId(User[] users) {
         return null;
     }
 
-    final long[] getUsersId(User[] users){
-         return null;
-    }
-
-    User[] deleteEmptyUsers(User[] users){
+    User[] deleteEmptyUsers(User[] users) {
         return null;
     }
 }
