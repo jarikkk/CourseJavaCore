b69fce94c5c53b552f4cf743db8c10ef06df59b8
yakovalenko <jaroslawkovalenko@jmail.com>
2016-10-18 15:44:13 +0300

    Task 7

--- Module7/src/task7_5/GenerateMethods.java
+++ Module7/src/task7_5/GenerateMethods.java
@@ -2,6 +2,7 @@ package task7_5;
 
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class GenerateMethods {
@@ -30,9 +31,33 @@ for (int j = 0; j < number; j++) {
     String nonce = sb.toString();
     arrayListresult.add(nonce);
 
-
         return arrayListresult;
     }
 
+public static LinkedList<Integer> generateMethodLinkedListInteger (int lenght) {
+    LinkedList<Integer> list = new LinkedList<Integer>();
+    for (int i = 0; i < lenght; i++) list.add(i + 1);
+    return list;
+}
 
+    public static LinkedList<String> generateMethodStringLinkedList(int number){
+
+        char[] chars = "0123456789!@#$%^&*()abcdefghijklmnopqrstuvwxyz".toCharArray();
+        LinkedList<String> list = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int j = 0; j < number; j++) {
+            for (int i = 0; i < 5; i++) {
+                char c = chars[random.nextInt(chars.length)];
+                sb.append(c);
+            }
+
+            sb.append(" | ");
+        }
+
+        String nonce = sb.toString();
+        list.add(nonce);
+
+        return list;
+    }
 }
--- Module7/src/task7_5/Main.java
+++ Module7/src/task7_5/Main.java
@@ -1,28 +1,40 @@
 package task7_5;
 
-import java.util.ArrayList;
-import java.util.Random;
-
-import static task7_5.GenerateMethods.generateMethodForInteger;
-import static task7_5.GenerateMethods.generateMethodString;
+import static task7_5.GenerateMethods.*;
 
 public class Main {
     public static void main(String[] args) {
+
         int a = (int) System.currentTimeMillis();
-        System.out.println(a);
         System.out.println(generateMethodForInteger(1000));
         int b = (int) System.currentTimeMillis();
-        System.out.println(b);
         System.out.println(generateMethodForInteger(10000));
         int c = (int) System.currentTimeMillis();
-        System.out.println(c);
         System.out.println(generateMethodString(1000));
         int d = (int) System.currentTimeMillis();
-        System.out.println(d);
         System.out.println(generateMethodString(10000));
         int e = (int) System.currentTimeMillis();
-        System.out.println(e);
+        System.out.println(generateMethodLinkedListInteger(1000));
+        int f = (int) System.currentTimeMillis();
+        System.out.println(generateMethodLinkedListInteger(10000));
+        int g = (int) System.currentTimeMillis();
+        System.out.println(generateMethodStringLinkedList(1000));
+        int h = (int) System.currentTimeMillis();
+        System.out.println(generateMethodStringLinkedList(10000));
+        int i = (int) System.currentTimeMillis();
 
+
+        System.out.println("Arraylist for Integer 1000 el: " + (b - a) + "ms");
+        System.out.println("Arraylist for Integer 10000 el: " + (c - b) + "ms");
+
+        System.out.println("Arraylist for String 1000 el: " + (d - c) + "ms");
+        System.out.println("Arraylist for String 10000 el: " + (e - d) + "ms");
+
+        System.out.println("LinkedList for Integer 1000 el: " + (f - e) + "ms");
+        System.out.println("LinkedList for Integer 10000 el: " + (g - f) + "ms");
+
+        System.out.println("LinkedList for String 1000 el: " + (h - g) + "ms");
+        System.out.println("LinkedList for String 10000 el: " + (i - h) + "ms");
 
     }
 
