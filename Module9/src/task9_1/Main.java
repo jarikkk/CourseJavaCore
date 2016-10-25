232bf5989df6d80a1b9fd335a714dc7a7398ab3f
yakovalenko <jaroslawkovalenko@jmail.com>
2016-10-25 12:30:52 +0300

    Task 9.1

--- Module9/src/task9_1/Main.java
+++ Module9/src/task9_1/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<Order> list = new ArrayList<>();
-
+        TreeSet<Order> set = new TreeSet();
 
         list.add(new Order(1, 10000, Currency.UAH, "ASUS", "Identificator1", new User(1, "Jack", "Jackson", "Kiev", 100000)));
         list.add(new Order(2, 1000, Currency.USD, "Lenovo", "Identificator2", new User(2, "Jack", "Jackson", "Kiev", 1000000)));
@@ -21,6 +21,17 @@ public class Main {
         list.add(new Order(8, 100000, Currency.USD, "Apple", "Identificator8", new User(8, "Jack", "Jackson", "Paris", 50000000)));
         list.add(new Order(10, 100, Currency.USD, "Razer", "Identificator9", new User(10, "Jack", "Jackson", "Kiev", 40000)));
         list.add(new Order(10, 100, Currency.USD, "Razer", "Identificator9", new User(10, "Jack", "Jackson", "Kiev", 40000)));
+
+        set.add(new Order(1, 10000, Currency.UAH, "ASUS", "Identificator1", new User(1, "Jack", "Jackson", "Kiev", 100000)));
+        set.add(new Order(2, 1000, Currency.USD, "Lenovo", "Identificator2", new User(2, "Jack", "Jackson", "Kiev", 1000000)));
+        set.add(new Order(3, 600000, Currency.UAH, "Sumsung", "Identificator3", new User(3, "Jack", "Jackson", "Kiev", 700000)));
+        set.add(new Order(4, 20000, Currency.USD, "Sparc", "Identificator4", new User(4, "Jack", "Jackson", "Berlin", 30000)));
+        set.add(new Order(5, 2000000, Currency.UAH, "hp", "Identificator5", new User(5, "Jack", "Jackson", "Berlin", 300000000)));
+        set.add(new Order(6, 7000, Currency.USD, "DELL", "Identificator6", new User(6, "Jack", "Jackson", "Berlin", 2453875)));
+        set.add(new Order(7, 300000, Currency.UAH, "SONY", "Identificator7", new User(7, "Jack", "Jackson", "Paris", 899000)));
+        set.add(new Order(8, 100000, Currency.USD, "Apple", "Identificator8", new User(8, "Jack", "Jackson", "Paris", 50000000)));
+        set.add(new Order(10, 100, Currency.USD, "Razer", "Identificator9", new User(10, "Jack", "Jackson", "Kiev", 40000)));
+        set.add(new Order(10, 100, Currency.USD, "Razer", "Identificator9", new User(10, "Jack", "Jackson", "Kiev", 40000)));
 
         System.out.println("List of unsorted orders: " + list);
         Collections.sort(list, (Order o1, Order o2) -> o1.getPrice() - o2.getPrice());
@@ -71,5 +82,7 @@ public class Main {
         System.out.println("\n" + "Lists separated with cities:");
         listsBycity.forEach((s, orders) -> lists.add(orders));
         lists.forEach((Order) -> System.out.println(Order));
+
+        
     }
 }
