d1ee6c402b2bcc56dad4816e369b5cdf2b755a39
yakovalenko <jaroslawkovalenko@jmail.com>
2016-11-15 17:09:05 +0200

    Task 11.2

--- Module11/src/task11_2/ClassReplaceWordsWriteToFile.java
+++ Module11/src/task11_2/ClassReplaceWordsWriteToFile.java
@@ -1,17 +1,21 @@
 package task11_2;
 
 
-
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ClassReplaceWordsWriteToFile {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
+
+        String oldString = "pop";
+        String newString = "!!!";
+        Map<String, String> stringsToReplace = new HashMap<>();
+        stringsToReplace.put(oldString, newString);
 
         ClassReplaceWordsWriteToFile classReplaceWords = new ClassReplaceWordsWriteToFile();
         BufferedReader br = classReplaceWords.readFromFile();
-        Map<String, String> replace = classReplaceWords.replaceWords("pop", "!!!", br);
+        Map<String, String> replace = classReplaceWords.replaceWords(stringsToReplace, br);
         classReplaceWords.filecontantReplacer(replace);
 
 
@@ -20,7 +24,7 @@ public class ClassReplaceWordsWriteToFile {
     public BufferedReader readFromFile() {
         BufferedReader br;
         try {
-            return br = new BufferedReader(new FileReader("C:/TEST.txt"));
+            return br = new BufferedReader(new FileReader("C:\\DATA\\HLAM\\TEST.txt"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("File is not found");
@@ -30,20 +34,26 @@ public class ClassReplaceWordsWriteToFile {
         return null;
     }
 
-    public Map<String, String> replaceWords(String oldString, String newString, BufferedReader bufferedReader) {
+    public Map<String, String> replaceWords(Map<String, String> stringsToReplace, BufferedReader bufferedReader) {
         Map<String, String> replacer = new HashMap<>();
         try {
             StringBuilder sb = new StringBuilder();
             String line = bufferedReader.readLine();
             while (line != null) {
-                if (!line.equals(oldString)) {
-                    sb.append(line.replace(oldString, newString));
-                    sb.append(System.lineSeparator());
+                String[] s = line.split(" ");
+                for (int i = 0; i < s.length; i++) {
+
+                    if (stringsToReplace.containsKey(s[i])) {
+                        sb.append(stringsToReplace.get(s[i]) + " ");
+                    } else {
+                        sb.append(s[i] + " ");
+                    }
                 }
                 line = bufferedReader.readLine();
             }
             String result = sb.toString();
-            replacer.put(oldString, result);
+
+            replacer.put(stringsToReplace.values().toArray()[0].toString(), result);
             return replacer;
 
 
@@ -57,13 +67,16 @@ public class ClassReplaceWordsWriteToFile {
         return null;
     }
 
-    public void filecontantReplacer(Map<String,String> writeReplace){
-try{
-        BufferedWriter bw = new BufferedWriter(new FileWriter("C:/TEST.txt"));
-        bw.write(String.valueOf(writeReplace.get("pop")));
-        bw.close();
-    } catch (IOException e) {
-        System.out.println("read failed");
-    }
+    public void filecontantReplacer(Map<String, String> writeReplace) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\DATA\\HLAM\\TEST.txt"));
+            for (String key :
+                    writeReplace.keySet()) {
+                bw.write(String.valueOf(writeReplace.get(key)));
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("read failed");
+        }
     }
 }
